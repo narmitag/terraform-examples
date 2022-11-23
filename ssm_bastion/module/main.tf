9ad77574c98d9631a840839bfedc3f0f0dc2a59e
Neil Armitage <neil.armitage@ensono.com>
2022-11-23 09:21:25 +0000

    Tests

--- ssm_bastion/module/main.tf
+++ ssm_bastion/module/main.tf
@@ -65,9 +65,6 @@ resource "aws_security_group" "allow_egress" {
   }
 }
 
-# data "template_file" "init" {
-#   template = file("${path.module}/cloud_init.init")
-# }
 
 ## Creating Launch Configuration
 resource "aws_launch_configuration" "this" {
@@ -76,7 +73,6 @@ resource "aws_launch_configuration" "this" {
   security_groups             = concat(aws_security_group.allow_egress.*.id, var.security_group_ids)
   associate_public_ip_address = false
   iam_instance_profile        = aws_iam_instance_profile.this.id
-  #user_data = data.template_file.init.rendered
   user_data = templatefile("${path.module}/cloud_init.init",{})
 
   lifecycle {
